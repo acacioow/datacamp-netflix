af5808a194fec56f888ec7b1d7c84297e8793ad7
acacioow <acaciobdemarco@gmail.com>
2022-02-01 10:12:55 -0300

    task-1 done

---
+++ dc_project.py
@@ -0,0 +1,12 @@
+#IMPORTS
+import numpy as np
+import pandas as pd
+
+#LIST
+years = [2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020]
+durations = [103, 101, 99, 100, 100, 95, 95, 96, 93, 90]
+
+#DICTIONARY
+movie_dict = {2011:103, 2012:101, 2013:99, 2014:100, 2015:100, 2016:95, 2017:95, 2018:96, 2019:93, 2020:90}
+
+print(movie_dict)
